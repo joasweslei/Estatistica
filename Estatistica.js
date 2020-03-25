a81826633086c29ecb4609757e33de1ad1fdec03
Mateus De Assis <aln13084.1@gmail.com>
2020-03-25 16:00:08 -0300

    substituição de variáveis por funçoes do próprio js, modificaçoes feitas no arquivo Estatistica.js: Compactação do codigo original

--- Estatistica.js
+++ Estatistica.js
@@ -1,14 +1,24 @@
+função  calcular () {
 
 
-function calcular(){
-    var nun;
-	var numero = document.getElementById("numero1").value;
-	var numero2 = document.getElementById("numero2").value;
-	var numero3 = document.getElementById("numero3").value;
-	//console.log(numero);
-	//console.log(numero2);
-	//console.log(numero3);
-	nun = numero*1 + numero2*1+ numero3*1 ;
-	//console.log(nun/3);
-	alert("a media dos numero e : "+ nun/3 );
+
+	
+
+	console.log( documento . getElementById ( "numero1" ) . valor );
+
+	console.log( documento . getElementById ( "numero2" ) . valor );
+
+	console.log( documento . getElementById ( "numero3" ) . valor );
+
+
+	
+
+	alerta ( "a media dos numero e:" +   console.log( ( 3numero * 1  +  numero2 * 1 +  numero3 * 1  ) / 3 ) ;
 }
+
+
+
+
+
+
+
