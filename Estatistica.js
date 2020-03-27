78d31eb264b9f68c6267a3d2fa6536eb7e3fabda
GustaDias <gustavo.ccomputacao@gmail.com>
2020-03-27 13:51:32 -0300

    Alteração da Função calcular (console.log)

--- Estatistica.js
+++ Estatistica.js
@@ -10,15 +10,9 @@ função  calcular () {
 
 	console.log( documento . getElementById ( "numero3" ) . valor );
 
+	console.log( documento . getElementById ( "numero4" ) . valor );
 
 	
 
 	alerta ( "a media dos numero e:" +   console.log( ( 3numero * 1  +  numero2 * 1 +  numero3 * 1  ) / 3 ) ;
 }
-
-
-
-
-
-
-
